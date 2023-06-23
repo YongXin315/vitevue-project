8462fc4ea4d5baed3b222c8b3ad75e5ae51dcfab
Yong Xin <xinchong909@gmail.com>
2023-06-23 19:38:50 +0800

    deleted:    firebase/init.js 	modified:   src/App.vue     deleted:    src/components/LoginForm.vue 	deleted:    src/components/SignupForm.vue

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,28 @@
 <script setup>
-import SignupForm from '@/components/SignupForm.vue'
-import LoginForm from '@/components/LoginForm.vue'
-import { auth } from'@/firebase/init.js'
-import { signOut } from 'firebase/auth'
 </script>
 
 <template>
-  <div v-if="!isLoggedIn">
-    <!-- login -->
-    <template v-if="showLogin">
-      <login-form @loggedIn="isLoggedIn= true"/>
-      <p>No account yet? <span @click="showLogin=false">Sign Up</span> instead.</p>
-    </template>
-    <!-- or register -->
-    <template v-else>
-      <signup-form @loggedIn="isLoggedIn= true"/>
-      
-      <p>Already registered? <span @click="showLogin=true">Login</span> instead.</p>
-    </template>
-  </div>
-  <!-- is logged in -->
-  <div v-else>
-    <h2>Welcome {{ displayName }}</h2>
-    <button @click="logOut">Sign Out</button>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
 </template>
 
-<script>
-export default {
-  components: { SignupForm, LoginForm },
-  data() {
-    return {
-      isLoggedIn: false,
-      showLogin: true,
-      displayName: ''
-    }
-  },
-  beforeUpdate(){
-    if(auth.currentUser){
-      this.displayName = auth.currentUser.displayName
-    }
-  },
-  methods:{
-    logOut(){
-      signOut(auth)
-      .then(() => {
-        this.isLoggedIn=false})
-    }
-  }
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-</script>
-
-<style>
-  p, h2 {
-    text-align: center;
-  }
-  span {
-    text-decoration: underline;
-    cursor: pointer;
-    color: blue;
-  }
-  form {
-    display: flex;
-    flex-direction: column;
-    row-gap: 16px;
-    max-width: 260px;
-    margin: 30px auto;
-  }
-  input, button {
-    height: 28px;
-
-  }
-  input {
-    padding-left: 10px;
-  }
-  button {
-    width: fit-content;
-    margin: auto;
-    border:2px solid black;
-    font-family: Arial, Helvetica, sans-serif;
-    font-weight: bold;
-    
-  }
-  p{
-    font-family: Arial, Helvetica, sans-serif;
-    font-weight: bold;
-  }
-  span{
-    font-weight:bold;
-  }
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
